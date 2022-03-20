128a25236c665a9e4455367ac093d0b6fda58c2a
Gooddevr <85499970+Gooddevr@users.noreply.github.com>
2022-03-21 00:17:57 +0300

    secKux

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as gddrFunctions from "./modules/function.js";
+
+gddrFunctions.isWebp();
+
+//Свайпер
+import Swiper, { Navigation, Pagination } from "swiper";
+const swiper = new Swiper();
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+let check = document.getElementsByName(terms_and_agreement);
+
+console.log(check);
